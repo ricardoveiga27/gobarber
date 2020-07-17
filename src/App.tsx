973598cde083a23f617c5a5fa39a91c1acaaacf4
Ricardo Veiga <ricardo.veiga@outlook.com.br>
2020-07-17 14:51:10 -0300

    pagina de login

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
 
 import GlobalStyle from './styles/global';
+import SingIn from './pages/SingIn/index';
 
 const App: React.FC = () => (
   <>
-    <h1>Hello World</h1>
+    <SingIn/>
     <GlobalStyle/>
   </>
 );
