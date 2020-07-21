11fe4d0742eedb17f7b4f16a5ae6777787152347
Ricardo Veiga <ricardo.veiga@outlook.com.br>
2020-07-21 19:25:55 -0300

    validação do singup com Yup

--- src/pages/SingUp/index.tsx
+++ src/pages/SingUp/index.tsx
@@ -1,6 +1,7 @@
-import React from 'react';
+import React, { useCallback } from 'react';
 import { FiArrowLeft, FiMail, FiUser, FiLock } from 'react-icons/fi';
 import { Form } from '@unform/web';
+import * as Yup from 'yup';
 
 import logoImg from '../../assets/logo.svg';
 import Input from '../../components/Input';
@@ -10,9 +11,25 @@ import { Background, Container, Content } from './styles';
 
 const SingUp: React.FC = () => {
   // eslint-disable-next-line @typescript-eslint/ban-types
-  function handleSubmit(data: object): void {
-    console.log(data);
-  }
+  const handleSubmit = useCallback(async (data: object) => {
+    try {
+      const schema = Yup.object().shape({
+        name: Yup.string().required('nome obrigatório'),
+        email: Yup.string()
+          .required('email obrigatório')
+          .email('digite um email válido'),
+        password: Yup.string()
+          .required('senha é obrigatória')
+          .min(6, 'a senha deve ter no minimo 6 caracteres'),
+      });
+
+      await schema.validate(data, {
+        abortEarly: false,
+      });
+    } catch (err) {
+      console.log(err);
+    }
+  }, []);
 
   return (
     <Container>
