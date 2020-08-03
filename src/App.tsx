3bec253674b3f092ff56e6c6d058160e4c5db196
Ricardo Veiga <ricardo.veiga@outlook.com.br>
2020-08-03 13:11:28 -0300

    login pelo contexto

--- src/App.tsx
+++ src/App.tsx
@@ -4,9 +4,13 @@ import GlobalStyle from './styles/global';
 import SingIn from './pages/SingIn/index';
 // import SingUp from './pages/SingUp/index';
 
+import { AuthProvider } from './context/AuthContext';
+
 const App: React.FC = () => (
   <>
-    <SingIn />
+    <AuthProvider>
+      <SingIn />
+    </AuthProvider>
     <GlobalStyle />
   </>
 );
