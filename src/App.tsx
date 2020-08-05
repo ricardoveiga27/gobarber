e2fcd01dd78d26a4932397e67941306ccec4ab1a
Ricardo Veiga <ricardo.veiga@outlook.com.br>
2020-08-05 14:54:51 -0300

    criado hook de Toasts

--- src/App.tsx
+++ src/App.tsx
@@ -3,16 +3,14 @@ import React from 'react';
 import GlobalStyle from './styles/global';
 import SingIn from './pages/SingIn/index';
 // import SingUp from './pages/SingUp/index';
-import ToastContainer from './components/ToastContainer';
-import { AuthProvider } from './hooks/AuthContext';
+
+import AuthProvider from './hooks/index';
 
 const App: React.FC = () => (
   <>
     <AuthProvider>
       <SingIn />
     </AuthProvider>
-
-    <ToastContainer />
 
     <GlobalStyle />
   </>
